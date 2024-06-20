5df567554941d1f1d60919dfeefca6a90962bdcb
JakubRumpca <jakub.rumpca@gmail.com>
2024-06-20 10:25:06 +0200

    Test case 5

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -5,6 +5,7 @@ describe('Users', () => {
   const standardUser = Cypress.env('STANDARD_USER');
   const lockedUser = Cypress.env('LOCKED_USER');
   const password = Cypress.env('PASSWORD');
+  const errorMessageLocator = '[data-test="error"]';
 
   beforeEach(() => {
     cy.visit('/');
@@ -17,6 +18,11 @@ describe('Users', () => {
   
   it('Should check if the locked user can not log in', () => {
     loginPage.login(lockedUser, password);
-    cy.get('[data-test="error"]').should('contain', 'Epic sadface: Sorry, this user has been locked out.');
+    cy.get(errorMessageLocator).should('contain', 'Epic sadface: Sorry, this user has been locked out.');
+  });
+
+  it('Should check if the standard user can not log in using the wrong password', () => {
+    loginPage.login(standardUser, "wrongPassword");
+    cy.get(errorMessageLocator).should('contain', 'Epic sadface: Username and password do not match any user in this service');
   });
 });
